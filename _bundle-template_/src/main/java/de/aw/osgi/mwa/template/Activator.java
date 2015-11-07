4ea58d3bd30f7e560d1ab1560432f52068ec57e1
Armin Weißer <armin.weisser@virtual-identity.com>
2015-11-07 23:41:27 +0100

    added basic packages and Dummy Bundle Acticator to _template-bundle_

---
+++ _bundle-template_/src/main/java/de/aw/osgi/mwa/template/Activator.java
@@ -0,0 +1,24 @@
+package de.aw.osgi.mwa.template;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+
+public class Activator implements BundleActivator {
+
+    /**
+     * Implements BundleActivator.start().
+     * @param bundleContext - the framework context for the bundle.
+     **/
+    public void start(BundleContext bundleContext) {
+        System.out.println("Dummy Service is online");
+    }
+
+    /**
+     * Implements BundleActivator.stop()
+     * @param bundleContext - the framework context for the bundle.
+     **/
+    public void stop(BundleContext bundleContext) {
+        System.out.println("Dummy Service is offline");
+    }
+}
